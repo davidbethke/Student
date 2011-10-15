ccb8be6e3852563f87be723ad1bb7f9cf319db94
Dave <david.bethke@gmail.com>
2011-10-14 23:46:43 -0500

    added tableview

--- CourseListWidget.cpp
+++ CourseListWidget.cpp
@@ -30,18 +30,35 @@ void CourseListWidget::init(){
     editCourse= new QLineEdit("Enter Course Title");
     comboHours= new QComboBox;
     buttonEnter= new QPushButton("Enter New Course");
+    courseList=new QStandardItemModel();
+    QStringList header;
+    header.append(QString("Title"));
+    header.append(QString("Credit Hours"));
+    courseList->setHorizontalHeaderLabels(header);
+    tableCourse = new QTableView;
+    tableCourse->setModel(courseList);
     QHBoxLayout *layout= new QHBoxLayout;
+    QVBoxLayout *vLayout= new QVBoxLayout;
     layout->addWidget(labelCourse);
     layout->addWidget(editCourse);
     layout->addWidget(labelHours);
     layout->addWidget(comboHours);
     layout->addWidget(buttonEnter);
-    setLayout(layout);
+    vLayout->addLayout(layout);
+    vLayout->addWidget(tableCourse);
+    setLayout(vLayout);
     listHours<<"5"<<"4"<<"3"<<"2"<<"1"<<"0";
 }
 
 void CourseListWidget::update(){
     //fill it in
-    QMessageBox::information(this,QString("Result"),QString("Course:"+editCourse->text()+" Hours:"+comboHours->itemText(comboHours->currentIndex())));
+    //QMessageBox::information(this,QString("Result"),QString("Course:"+editCourse->text()+" Hours:"+comboHours->itemText(comboHours->currentIndex())));
+    QStandardItem *title= new QStandardItem(editCourse->text());
+    QStandardItem *hours = new QStandardItem(comboHours->itemText(comboHours->currentIndex()));
+    QList<QStandardItem *> list;
+    list << title<< hours;
+    //courseList->setItem(0,0,title);
+    //courseList->setItem(0,1,hours);
+    courseList->appendRow(list);
     
 }
